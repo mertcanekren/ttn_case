e12a9c051b93f59f104cfb87201ab96086a4363c
Mertcan Ekren <mertcanekren@gmail.com>
2023-04-13 14:47:10 +0300

    Kampanya oluşturma dosyasında olan sorgu güncellendi

--- create_campaign.php
+++ create_campaign.php
@@ -12,26 +12,42 @@ header('Content-Type: application/json; charset=utf-8');
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
     $name = $_POST['name'];
-    $type = $_POST['type'];
-    $min_purchase_amount = $_POST['min_purchase_amount'];
-    $discount_percentage = $_POST['discount_percentage'];
-    $free_item_quantity = $_POST['free_item_quantity'];
+    $discount_type = $_POST['discount_type'];
+    $discount_amount = $_POST['discount_amount'];
+    $minimum_campaign_amount = $_POST['minimum_campaign_amount'];
+    $campaign_author = $_POST['campaign_author'];
+    $campaign_minimum_pieces = $_POST['campaign_minimum_pieces'];
+   
 
     // Veritabanına kampanya bilgilerini kaydet
-    $sql = "INSERT INTO campaigns (name, type, min_purchase_amount, discount_percentage, free_item_quantity) VALUES (:name, :type, :min_purchase_amount, :discount_percentage, :free_item_quantity)";
+    $sql = "INSERT INTO campaigns (name, discount_type, discount_amount, minimum_campaign_amount, campaign_author, campaign_minimum_pieces) VALUES (:name, :discount_type, :discount_amount, :minimum_campaign_amount,:campaign_author, :campaign_minimum_pieces)";
     $campaign_insert = $pdo->prepare($sql);
     $campaign_insert->bindParam(':name', $name);
-    $campaign_insert->bindParam(':type', $type);
-    $campaign_insert->bindParam(':min_purchase_amount', $min_purchase_amount);
-    $campaign_insert->bindParam(':discount_percentage', $discount_percentage);
-    $campaign_insert->bindParam(':free_item_quantity', $free_item_quantity);
+    $campaign_insert->bindParam(':discount_type', $discount_type);
+    $campaign_insert->bindParam(':discount_amount', $discount_amount);
+    $campaign_insert->bindParam(':minimum_campaign_amount', $minimum_campaign_amount);
+    $campaign_insert->bindParam(':campaign_author', $campaign_author);
+    $campaign_insert->bindParam(':campaign_minimum_pieces', $campaign_minimum_pieces);
     $campaign_insert->execute();
 
     // Yeni kampanya eklenmişse, 201 Created HTTP durum kodu ve yeni kaydın ID'siyle birlikte bir JSON yanıtı döndür
     if ($campaign_insert->rowCount() > 0) {
         $new_campaign_id = $pdo->lastInsertId();
         http_response_code(201);
-        echo json_encode(array("status" => true, "data" => ["id" => $new_campaign_id, "name" => $name, "type" => $type, "min_purchase_amount" => $min_purchase_amount, "discount_percentage" => $discount_percentage, "free_item_quantity" => $free_item_quantity]));
+        echo json_encode(array(
+            "status" => true,
+            "data" => 
+                [
+                    "id" => $new_campaign_id,
+                    "name" => $name,
+                    "discount_type" => $discount_type,
+                    "discount_amount" => $discount_amount,
+                    "minimum_campaign_amount" => $minimum_campaign_amount,
+                    "campaign_author" => $campaign_author,
+                    "campaign_minimum_pieces" => $campaign_minimum_pieces
+                ]
+            )
+        );
     } else {
         // Eğer yeni kampanya eklenememişse, 500 Internal Server Error HTTP durum kodu ve hata mesajıyla birlikte bir JSON yanıtı döndür
         http_response_code(500);
