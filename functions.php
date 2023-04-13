6be0fdab539f30e80191812d59ac6167026f1509
Mertcan Ekren <mertcanekren@gmail.com>
2023-04-13 16:16:20 +0300

    sipariş gösterim sayfası eklendi

--- functions.php
+++ functions.php
@@ -41,6 +41,107 @@ function getBookDatabyID($book_id){
 }
 
 /**
+* id bilgisi gönderilen müşteri bilgilerini döndürür.
+*
+* @param int $customer_id müşteri id bilgisi
+* @return array
+*/
+function getCustomerDatabyID($customer_id){
+    global $pdo;
+
+    $customer_query = "SELECT * FROM customers WHERE id=:id";
+    $customer_query = $pdo->prepare($customer_query);
+    $customer_query->execute(['id' => $customer_id]);
+    if ($customer_query->rowCount() > 0) {
+        $customer_data = $customer_query->fetch(PDO::FETCH_ASSOC);
+        return $customer_data;
+    }else{
+        return false;
+    }
+}
+
+/**
+* id bilgisi gönderilen kampanya bilgilerini döndürür.
+*
+* @param int $campaign_id kampanya id bilgisi
+* @return array
+*/
+function getCampaignDatabyID($campaign_id){
+    global $pdo;
+
+    $campaign_query = "SELECT * FROM campaigns WHERE id=:id";
+    $campaign_query = $pdo->prepare($campaign_query);
+    $campaign_query->execute(['id' => $campaign_id]);
+    if ($campaign_query->rowCount() > 0) {
+        $campaign_data = $campaign_query->fetch(PDO::FETCH_ASSOC);
+        return $campaign_data;
+    }else{
+        return false;
+    }
+}
+
+/**
+* id bilgisi gönderilen siparişin ürünlerini döndürür.
+*
+* @param int $order_id sipariş id bilgisi
+* @return array
+*/
+function getOrderProductsbyOrderID($order_id){
+    global $pdo;
+
+    $order_p_query = "SELECT * FROM order_products WHERE order_id=:order_id";
+    $order_p_query = $pdo->prepare($order_p_query);
+    $order_p_query->execute(['order_id' => $order_id]);
+    if ($order_p_query->rowCount() > 0) {
+        $order_p_data = $order_p_query->fetchAll(PDO::FETCH_ASSOC);
+        return $order_p_data;
+    }else{
+        return false;
+    }
+}
+
+/**
+* id bilgisi gönderilen ürünün bilgilerini döndürür.
+*
+* @param int $product_id ürün id bilgisi
+* @param string $field istenilen sütun adı
+* @return array
+*/
+function getProductbyID($product_id){
+    global $pdo;
+
+    $product_query = "SELECT * FROM books WHERE id=:id";
+    $product_query = $pdo->prepare($product_query);
+    $product_query->execute(['id' => $product_id]);
+    if ($product_query->rowCount() > 0) {
+        $product_data = $product_query->fetch(PDO::FETCH_ASSOC);
+        return $product_data;
+    }else{
+        return false;
+    }
+}
+
+/**
+* id bilgisi gönderilen yazarın bilgilerini döndürür.
+*
+* @param int $author_id yazar id bilgisi
+* @return array
+*/
+function getAuthorbyID($author_id){
+    global $pdo;
+
+    $author_query = "SELECT * FROM authors WHERE id=:id";
+    $author_query = $pdo->prepare($author_query);
+    $author_query->execute(['id' => $author_id]);
+    if ($author_query->rowCount() > 0) {
+        $author_data = $author_query->fetch(PDO::FETCH_ASSOC);
+        return $author_data;
+    }else{
+        return false;
+    }
+}
+
+/**
 * sipariş aşamasında ürün arrayinin sıralanması için kullanılır
 */
 function sort_list_price($a, $b) {
--- show_order.php
+++ show_order.php
@@ -7,15 +7,84 @@
 */
 
 include 'db_connect.php';
+include 'functions.php';
+header('Content-Type: application/json; charset=utf-8');
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET'){
-    if($_GET["order_id"] != ""){
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+    if($_POST["order_id"] != ""){
 
         $order_sql = $pdo->prepare("SELECT * FROM orders where id=:order_id");
-        $order_sql->bindParam(':order_id', $_GET["order_id"]);
+        $order_sql->bindParam(':order_id', $_POST["order_id"]);
         $order_sql->execute();
-        $order_data = $order_sql->fetch(PDO::FETCH_ASSOC);
-        print_r($order_data);
+        if ($order_sql->rowCount() > 0) {
+
+            $order_data = $order_sql->fetch(PDO::FETCH_ASSOC);
+            $order_return_data = array();
+
+            // siparişin müşteri bilgileri çekiliyor
+            $customer_data = getCustomerDatabyID($order_data["customer_id"]);
+
+            // eğer sipariş bir kampanyadan faydalanmışsa
+            if($order_data["campaign_id"] != 0){
+                //siparişin kampanya bilgileri çekiliyor
+                $campaign_data = getCampaignDatabyID($order_data["campaign_id"]);
+                $order["campaign"] = $campaign_data["name"];
+            }else{
+                $order["campaign"] = "Faydalanılan kampanya yok.";
+            }
+
+            // sipariş ürün bilgileri
+            $order_products = getOrderProductsbyOrderID($order_data["id"]);
+
+            $products = array();
+            foreach ($order_products as &$order_products_row) {
+                
+                // sipariş ürün bilgileri okunuyor
+                $product_data = getProductbyID($order_products_row["product_id"]);
+
+                // ürünün yazar bilgisi
+                $author_data = getAuthorbyID($product_data["id"]);
+
+                $p_temp["title"] = $product_data["title"];
+                $p_temp["quantity"] = $order_products_row["quantity"];
+                $p_temp["author_id"] = $author_data["author"];
+                $p_temp["list_price"] = $order_products_row["list_price"];
+                $products[] = $p_temp;
+            }
+            
+            //sipariş toplam ücreti
+            $order["total_price"] = $order_data["total_price"];
+            
+            // siparişe uygulanmış bir indirim varsa ücreti
+            $order["discounted_price"] = $order_data["discounted_price"];
+
+            //siparişe indirim uygulanmışsa, indirimsiz ücreti
+            $order["without_discounted_price"] = $order_data["without_discounted_price"];
+            
+            // sipariş kargo ücreti
+            $order["shipping_cost"] = $order_data["shipping_cost"];
+
+            // siparişin müşteri id bilgisi
+            $order["customer_id"] = $order_data["customer_id"];
+
+            // siparişin kampanya id bilgisi
+            $order["campaign_id"] = $order_data["campaign_id"];
+
+            $order_return_data["order_data"] = $order;
+            $order_return_data["customer"] = $customer_data;
+            $order_return_data["products"] = $products;
+            http_response_code(200);
+            echo json_encode(array(
+                "status" => true,
+                "order_data" => $order_return_data
+            ));
+        }else{
+            http_response_code(400);
+            echo json_encode(array("status" => false,"message" => "Sipariş bulunamadı."));        
+        }
+    }else{
+        http_response_code(400);
+        echo json_encode(array("status" => false,"message" => "Sipariş numarası gönderiniz."));    
     }
 }else{
     http_response_code(400);
